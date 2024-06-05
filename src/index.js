313f041b363235e7d682577f52349284ec739bc2
Dhruv Arora <dhruvarora.code@gmail.com>
2024-06-05 13:11:08 +0530

    implemented loan

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import store from "./store";
 
 store.dispatch({ type: "account/deposit", payload: 250 });
-console.log(store.getState());
+// console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      {/* now are application knows about the redux store, which every single component in the application can now read data from the store and can dispatch actions to it, so it's basically broadcasting global state into every component that actually wants to read it */}
+      <App />
+    </Provider>
   </React.StrictMode>
 );
