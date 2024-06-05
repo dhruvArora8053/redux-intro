a9f337395610d62b02dc5c36a337c1622e1c2273
Dhruv Arora <dhruvarora.code@gmail.com>
2024-06-05 14:57:37 +0530

    setup middleware and thunk

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -2,12 +2,17 @@ const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
+  isLoading: false,
 };
 
 export default function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
-      return { ...state, balance: state.balance + action.payload };
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+        isLoading: false,
+      };
 
     case "account/withdraw":
       return { ...state, balance: state.balance - action.payload };
@@ -28,14 +33,37 @@ export default function accountReducer(state = initialStateAccount, action) {
         loanPurpose: "",
         balance: state.balance - state.loan,
       };
+
+    case "account/convertingCurrency":
+      return {
+        ...state,
+        isLoading: true,
+      };
+
     default:
       return state;
   }
 }
 
-export function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  // Thunk MiddleWare:
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    // API call
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+
+    // Return action
+    dispatch({ type: "account/deposit", payload: converted });
+  };
+  // first we dispatch at the deposit function and in the case we have another currency that is not US dollars then that deposit function will return a function, so the above function right here and so then redux will call this function and pass it the dispatch function so that we can then call that function in the future when we actually want to dispatch our action in the end to the store so where we have already the result of asynchrnous operation
 }
+
 export function withdraw(amount) {
   return { type: "account/withdraw", payload: amount };
 }
